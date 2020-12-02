7c02c3d5c330e4c62794fabcde37c59c7dcd6a3e
vp <vpatel@localhost.localdomain>
2020-12-01 23:56:00 -0800

    - Implemented Login Failure test

--- main/client/src/components/__tests__/login.js
+++ main/client/src/components/__tests__/login.js
@@ -7,7 +7,11 @@ describe('Selenium Login Tests', () => {
 
   beforeEach(async () => {
     driver = await new Builder().forBrowser('firefox').build(); // can be switched to any browser on your PATH var
+
     await driver.get("http://localhost:3000/"); // assumes server and client are active
+
+    // set window size
+    await driver.manage().window().setRect(1750, 1000);
   }, 10000);
 
   afterEach(async () => {
@@ -15,9 +19,7 @@ describe('Selenium Login Tests', () => {
   }, 10000);
 
   it('Login Success', async () => {
-    // set the window size
-    await driver.manage().window().setRect(1750, 1000);
-
+      
     // find email field, click and enter arbitrary email
     await driver.findElement(By.name("email")).click();
     await driver.findElement(By.name("email")).sendKeys("test@test.com");
@@ -31,6 +33,24 @@ describe('Selenium Login Tests', () => {
 
     // expect an existing logout button (indicates we are logged in)
     expect(buttonAttr).toEqual("logout-button");
+
+  }, 10000);
+
+  it('Login Failure', async () => {
+
+    // find email field, click and enter arbitrary email
+    await driver.findElement(By.name("email")).click();
+    await driver.findElement(By.name("email")).sendKeys("test");
+
+    // click login button
+    await driver.findElement(By.tagName("button")).click(); // login button
+
+    // get the next button (should be the same button in this case)
+    const button = await driver.findElement(By.tagName("button"));
+    const buttonText = await button.getText();
+
+    // expect to stay on log in screen
+    expect(buttonText).toEqual("LOG IN"); // would be better to look for alert message...
 
   }, 10000);
 });
