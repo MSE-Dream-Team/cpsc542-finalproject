c619688216f0105ce77d697e5287a3bb86ccd662
vp <vpatel@localhost.localdomain>
2020-12-04 01:44:33 -0800

    - Completed 'Add Multiple Items' test

--- main/client/src/pages/__tests__/cart.js
+++ main/client/src/pages/__tests__/cart.js
@@ -2,8 +2,9 @@ const { until, Builder, By } = require("selenium-webdriver");
 
 jest.setTimeout(30000); // ugly hack
 
-describe("Selenium Login Tests", () => {
+describe("Selenium Cart Tests", () => {
   let driver;
+  let HOST = "http://localhost:3000";
 
   // open the browser and login before executing all tests
   beforeAll(async () => {
@@ -31,34 +32,39 @@ describe("Selenium Login Tests", () => {
   const getElementByHref = async (hrefVal) => {
     const elements = await driver.findElements(By.tagName("a"));
     for (var i = 0; i < elements.length; ++i) {
+      // console.log(`${HOST}${hrefVal}`);
       const attribute = await elements[i].getAttribute("href");
-      if (attribute.includes(hrefVal)) {
+      if (`${HOST}${hrefVal}` == attribute) {
         return elements[i];
       }
     }
     return null;
   };
 
-  it("Add Single Item", async () => {
-    // wait until the trips are displayed
-    await driver.wait(until.elementLocated(By.className("css-10q0vj5")), 5000);
-
-    const starLink = await getElementByHref("109"); // starLink = launch-109
-    if (!starLink) throw new Error("Launch 109 not found");
-    starLink.click();
-
+  const timeout = async (milli) => {
     await new Promise((res) =>
       setTimeout(() => {
         expect(true).toBe(true);
         res();
-      }, 1000)
-    ); // wait one second to avoid 'stale' error`
+      }, milli)
+    );
+  };
+
+  it("Add Single Item", async () => {
+    // wait until the trips are displayed
+    await driver.wait(until.elementLocated(By.className("css-10q0vj5")), 5000);
+
+    const starLink = await getElementByHref("/launch/109"); // starLink = launch-109
+    if (!starLink) throw new Error("Launch 109 not found");
+    starLink.click();
+
+    await timeout(1000); // wait one second to avoid 'stale' error`
 
     // add starLink to cart (first button is add to cart)
     driver.findElement(By.tagName("button")).click();
 
     // navigate to cart
-    const cartButton = await getElementByHref("cart");
+    const cartButton = await getElementByHref("/cart");
     if (!cartButton) throw new Error("Cart Button not found");
     cartButton.click();
 
@@ -70,5 +76,72 @@ describe("Selenium Login Tests", () => {
 
     // expect Starlink-15 in cart
     expect(text).toEqual(expect.stringContaining("Starlink-15"));
+  });
+
+  it("Add Multiple Items", async () => {
+    // continuing from previous test, navigate to home page
+    const homeButton = await getElementByHref("/");
+    if (!homeButton) throw new Error("Home Button not found");
+    homeButton.click();
+
+    // wait until the trips are displayed
+    await driver.wait(until.elementLocated(By.className("css-10q0vj5")), 5000);
+
+    const sentinel = await getElementByHref("/launch/108"); // Sentinel = launch-108
+    if (!sentinel) throw new Error("Launch 108 not found");
+    sentinel.click();
+    await timeout(1000);
+
+    // add sentinel to cart (first button is add to cart)
+    driver.findElement(By.tagName("button")).click();
+    await timeout(1000);
+
+    // navigate to home page
+    homeButton.click();
+    await timeout(1000);
+
+    // scroll down to view launch 107
+    await driver.executeScript("window.scroll(0,250)");
+
+    const crew1 = await getElementByHref("/launch/107"); // Crew-1 = launch-107
+    if (!crew1) throw new Error("Launch 107 not found");
+    crew1.click();
+    await timeout(1000);
+
+    // add crew1 to cart (first button is add to cart)
+    driver.findElement(By.tagName("button")).click();
+    await timeout(1000);
+
+    // navigate to cart
+    const cartButton = await getElementByHref("/cart");
+    if (!cartButton) throw new Error("Cart Button not found");
+    cartButton.click();
+
+    // array with expected items
+    const expectedResults = ["Starlink-15", "Sentinel-6", "Crew-1"];
+
+    // find div containing a list of cart items
+    const div = await driver.findElement(By.className("css-1on771l"));
+
+    // get each item from the cart
+    const cartItems = await div.findElements(By.tagName("a"));
+
+    // new array containing items as text
+    const cartItemTexts = await Promise.all(
+      cartItems.map(async (item) => {
+        return await item.getText();
+      })
+    );
+
+    // check for expected items
+    expect(cartItemTexts[0]).toEqual(
+      expect.stringContaining(expectedResults[0])
+    );
+    expect(cartItemTexts[1]).toEqual(
+      expect.stringContaining(expectedResults[1])
+    );
+    expect(cartItemTexts[2]).toEqual(
+      expect.stringContaining(expectedResults[2])
+    );
   });
 });
